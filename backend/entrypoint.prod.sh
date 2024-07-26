84e24adef79f55880d816ccddda32ea173afa442
Rostislav <rostislav444@gmail.com>
2024-07-27 01:17:08 +0300

    Updated Dockerfile.prod

--- backend/entrypoint.prod.sh
+++ backend/entrypoint.prod.sh
@@ -11,13 +11,9 @@ then
     echo "PostgreSQL started"
 fi
 
-# Collect static files
-echo "Collecting static files"
-python manage.py collectstatic --noinput
-
-# Apply database migrations
-echo "Applying database migrations"
 python manage.py migrate
 
-# Start server
-exec gunicorn project.wsgi:application --bind 0.0.0.0:8000 --workers 3 --log-level=info
+# change owner of celerybeat-schedule file to app user
+chown app:app /home/app/web/celerybeat-schedule
+
+exec "$@"
