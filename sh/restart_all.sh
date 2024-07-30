90554783b2534021b5fa87724d792b95023569c8
Rostislav <rostislav444@gmail.com>
2024-07-30 13:05:59 +0300

    Sh change

--- sh/restart_all.sh
+++ sh/restart_all.sh
@@ -2,3 +2,5 @@ docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up --build -d
 docker compose -f docker-compose.nginx.yml down nginx
 docker compose -f docker-compose.nginx.yml up nginx --build -d
+docker compose -f docker-compose.frontend.yml down
+docker compose -f docker-compose.frontend.yml up --build -d
