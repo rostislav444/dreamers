0857d805866960d74be852dc6f8e779e8d4ef542
Rostislav <rostislav444@gmail.com>
2024-08-28 20:25:22 +0300

    minor changes

---
+++ sh/restart_frontend.sh
@@ -0,0 +1,2 @@
+docker compose -f docker-compose.frontend.yml down
+docker compose -f docker-compose.frontend.yml up --build -d
---
+++ sh/restart_nginx.sh
@@ -0,0 +1,2 @@
+docker compose -f docker-compose.nginx.yml down nginx
+docker compose -f docker-compose.nginx.yml up nginx --build -d
