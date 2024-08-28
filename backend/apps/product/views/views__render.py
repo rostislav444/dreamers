d6b68645854f96a11700e4cc9120f538cffb3742
Rostislav <rostislav444@gmail.com>
2024-08-28 22:02:18 +0300

    minor changes

--- backend/apps/product/views/views__render.py
+++ backend/apps/product/views/views__render.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, mixins, generics
+from rest_framework.decorators import action
 
 from apps.product.models import SkuImages, ProductPartSceneMaterialImage, Product, CameraInteriorLayerMaterialGroup, \
     CameraInteriorLayerMaterialImage, CameraInteriorLayerMaterial
@@ -12,6 +13,10 @@ class ProductRenderViewSet(generics.GenericAPIView, mixins.RetrieveModelMixin, v
 
     def get_queryset(self):
         return Product.objects.all()
+
+    @action(detail=True, methods=['get'])
+    def get_all_product_ids(self, request, *args, **kwargs):
+        return Product.objects.all().values_list('id', flat=True)
 
 
 class LoadSkuImageView(generics.GenericAPIView, mixins.CreateModelMixin, viewsets.ViewSet):
